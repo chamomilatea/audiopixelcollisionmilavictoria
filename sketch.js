f5366b9a028b32cbc006965fcb17d1958bf82202
chamomilatea <cdelapuente21@gmail.com>
2024-10-24 14:29:56 -0500

    more small changes

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var mic; // to get audio input from mic
+var spacing = 100;
+var circleSize;
+var col1, col2, col3;
+var images = []; // Array to hold images
+var currentImage; // To hold the current image
+var lastBeatTime = 0;
+var beatThreshold = 0.1; // Adjust this threshold for beat detection
+
+function preload() {
+  // Load your specific images
+  images.push(loadImage('images/frameone.jpg'));
+  images.push(loadImage('images/frametwo.jpg'));
+  images.push(loadImage('images/framethree.jpg'));
+  images.push(loadImage('images/framefour.jpg'));
+}
+
+function setup() {
+  createCanvas(900, 600);
+  
+  mic = new p5.AudioIn();
+  mic.start();
+  
+  col1 = color(random(255), 100, 0);
+  col2 = color(100, random(255), 0);
+  col3 = color(100, 0, random(255));
+  
+  currentImage = random(images); // Load a random image at start
+}
+
+function draw() {
+  background(0);
+  
+  // Audio level from mic
+  var vol = mic.getLevel();
+  circleSize = map(vol, 0, 1, 25, 200);
+  
+  // Draw the current image
+  image(currentImage, width / 2 - currentImage.width / 2, height / 2 - currentImage.height / 2);
+  
+  noFill(); 
+  for (var x = 0; x <= width; x += spacing) {
+    for (var y = 0; y <= height; y += spacing) {
+      stroke(col1);
+      ellipse(x, y, circleSize, circleSize);
+      stroke(col2);
+      ellipse(x, y, circleSize / 2, circleSize / 2);
+      stroke(col3);
+      ellipse(x, y, circleSize + 20, circleSize + 20);
+    }
+  }
+  
+  // Check for beat detection
+  if (vol > beatThreshold && millis() - lastBeatTime > 300) { // 300ms gap for beat detection
+    lastBeatTime = millis();
+    currentImage = random(images); // Load a new random image on beat
+  }
+}
+
+function mousePressed() {
+  col1 = color(random(255), 100, 0);
+  col2 = color(100, random(255), 0);
+  col3 = color(100, 0, random(255));
+}
